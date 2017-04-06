44d6febcf69348d666d90ce135b63397dd3d2f9a
Byung Kook Hwang <bkhwang82@gmail.com>
2017-04-06 12:10:14 +0900

    init project

---
+++ RxTodoMVP/Pods/Target Support Files/Pods-RxTodoMVP/Pods-RxTodoMVP-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxTodoMVP : NSObject
+@end
+@implementation PodsDummy_Pods_RxTodoMVP
+@end
---
+++ RxTodoMVP/RxTodoMVP/Presentations/Main/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxTodoMVP
+//
+//  Created by Byung Kook Hwang on 2017. 4. 6..
+//  Copyright © 2017년 magi82. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
